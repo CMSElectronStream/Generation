81d2b5681ecebe726498086f2948a03094ac5468
Andrea <massironi.andrea@gmail.com>
2013-11-05 15:01:02 +0100

    update

--- README.md
+++ README.md
@@ -5,13 +5,39 @@ Generation of signal
 
 W>enu
 
+git-cms-addpkg Configuration/Generator
 
-cmsDriver.py WToENu_TuneZ2star_13TeV_pythia6_cff.py \
-   --step GEN,SIM --beamspot Realistic13TeVCollision \
-   --conditions START62_V1::All \
-   --pileup NoPileUp \
-   --datamix NODATAMIXER \
-   --eventcontent RAWSIM \
-   --datatier GEN-SIM \
-   -n 200000 \
-   --no_exec
+cp Generation/WToENu_TuneZ2star_13TeV_pythia6_cff.py Configuration/Generator/python/
+scramv1 b -j 8
+
+    cmsDriver.py Configuration/Generator/python/WToENu_TuneZ2star_13TeV_pythia6_cff.py \
+    --step GEN,SIM --beamspot Realistic8TeVCollision \
+    --conditions START62_V1::All \
+    --pileup NoPileUp \
+    --datamix NODATAMIXER \
+    --eventcontent RAWSIM \
+    --datatier GEN-SIM \
+    -n 200000 \
+    --no_exec
+
+
+
+
+    cmsDriver.py WE_14TeV_cfi.py \
+    --step GEN,SIM --beamspot Realistic8TeVCollision \
+    --conditions START62_V1::All \
+    --pileup NoPileUp \
+    --datamix NODATAMIXER \
+    --eventcontent RAWSIM \
+    --datatier GEN-SIM \
+    -n 200000 \
+    --no_exec
+
+
+
+To be fixed:
+
+    Realistic13TeVCommision
+
+
+cmsRun WToENu_TuneZ2star_13TeV_pythia6_cff_py_GEN_SIM.py
--- WToENu_TuneZ2star_13TeV_pythia6_cff.py
+++ WToENu_TuneZ2star_13TeV_pythia6_cff.py
@@ -3,38 +3,32 @@ import FWCore.ParameterSet.Config as cms
 from Configuration.Generator.PythiaUEZ2starSettings_cfi import *
 
 generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(1.0),
     pythiaHepMCVerbosity = cms.untracked.bool(False),
-    maxEventsToPrint = cms.untracked.int32(0),
-    pythiaPylistVerbosity = cms.untracked.int32(1),
-    filterEfficiency = cms.untracked.double(1.),
-    crossSection = cms.untracked.double(9.181e3),
+    crossSection = cms.untracked.double(16540.0),
     comEnergy = cms.double(13000.0),
+    maxEventsToPrint = cms.untracked.int32(0),
     PythiaParameters = cms.PSet(
         pythiaUESettingsBlock,
-        processParameters = cms.vstring('MSEL        = 0    !User defined processes', 
-                                        'MSUB(2)     = 1    !W production', 
-                                        'MDME(190,1) = 0    !W decay into dbar u', 
-                                        'MDME(191,1) = 0    !W decay into dbar c', 
-                                        'MDME(192,1) = 0    !W decay into dbar t', 
-                                        'MDME(194,1) = 0    !W decay into sbar u', 
-                                        'MDME(195,1) = 0    !W decay into sbar c', 
-                                        'MDME(196,1) = 0    !W decay into sbar t', 
-                                        'MDME(198,1) = 0    !W decay into bbar u', 
-                                        'MDME(199,1) = 0    !W decay into bbar c', 
-                                        'MDME(200,1) = 0    !W decay into bbar t', 
-                                        'MDME(205,1) = 0    !W decay into bbar tp', 
-                                        'MDME(206,1) = 1    !W decay into e+ nu_e', 
-                                        'MDME(207,1) = 0    !W decay into mu+ nu_mu', 
-                                        'MDME(208,1) = 0    !W decay into tau+ nu_tau'),
+        processParameters = cms.vstring(
+            'MSEL        = 0    !User defined processes', 
+            'MSUB(2)     = 1    !W production', 
+            'MDME(190,1) = 0    !W decay into dbar u', 
+            'MDME(191,1) = 0    !W decay into dbar c', 
+            'MDME(192,1) = 0    !W decay into dbar t', 
+            'MDME(194,1) = 0    !W decay into sbar u', 
+            'MDME(195,1) = 0    !W decay into sbar c', 
+            'MDME(196,1) = 0    !W decay into sbar t', 
+            'MDME(198,1) = 0    !W decay into bbar u', 
+            'MDME(199,1) = 0    !W decay into bbar c', 
+            'MDME(200,1) = 0    !W decay into bbar t', 
+            'MDME(206,1) = 1    !W decay into e+ nu_e', 
+            'MDME(207,1) = 0    !W decay into mu+ nu_mu', 
+            'MDME(208,1) = 0    !W decay into tau+ nu_tau'
+            ),
         # This is a vector of ParameterSet names to be read, in this order
         parameterSets = cms.vstring('pythiaUESettings', 
             'processParameters')
     )
-)
-
-
-configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.3 $'),
-    name = cms.untracked.string('$Source: /afs/cern.ch/project/cvs/reps/CMSSW/CMSSW/Configuration/GenProduction/python/EightTeV/WToENu_TuneZ2star_13TeV_pythia6_cff.py,v $'),
-    annotation = cms.untracked.string('PYTHIA6-Wenu Tune Z2* at 13 TeV')
 )
