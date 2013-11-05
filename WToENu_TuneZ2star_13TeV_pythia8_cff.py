dde299b6f363f6422d7ede655fda62fb080494e8
Andrea <massironi.andrea@gmail.com>
2013-11-05 16:23:56 +0100

    py8 new fragment

---
+++ WToENu_TuneZ2star_13TeV_pythia8_cff.py
@@ -0,0 +1,27 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.PythiaUEZ2starSettings_cfi import *
+
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    crossSection = cms.untracked.double(71.39e+09),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(8000.0),
+    PythiaParameters = cms.PSet(
+        processParameters = cms.vstring(
+	   'Main:timesAllowErrors = 10000',
+           'ParticleDecays:limitTau0 = on',
+           'ParticleDecays:tauMax = 10',
+           'Tune:ee 3',
+           'Tune:pp 2',
+
+           'WeakSingleBoson:ffbar2W = on',
+           '24:onMode = off',
+           '24:onIfAny = 11 12',
+          ),
+        parameterSets = cms.vstring('processParameters')
+     )
+)
